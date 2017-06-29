70d9476247ead2f0b2fd7ce98ff9fbfdbe49be1c
hari <hari@qburst.com>
2017-06-29 10:28:23 +0530

    some changes - checking out to testing env setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import '../assets/css/main.scss';
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
-export class AppComponent { }
+
+export class AppComponent { 
+	title = 'Tour of Heroes';
+  	hero: Hero = {
+  		id: 1,
+  		name: 'Windstorm'
+	};
+}
+
+export class Hero {
+  	id: number;
+  	name: string;
+}
