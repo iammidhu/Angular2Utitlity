a22370626e9f9aba6feef87c89526b17fb9dedb9
Midhun <midhun@qburst.com>
2017-07-03 19:08:22 +0530

    home screen styling in progress

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,18 +2,20 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { RouterModule }   from '@angular/router';
- 
+import { MdlModule } from '@angular-mdl/core';
+
 import { HomeComponent }     from './home.component';
- 
+
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  declarations: [
-    HomeComponent
-  ],
-  bootstrap: [ HomeComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        MdlModule
+    ],
+    declarations: [
+        HomeComponent
+    ],
+    bootstrap: [HomeComponent]
 })
 
-export class HomeModule {}
+export class HomeModule { }
--- src/app/theme/header/header.component.ts
+++ src/app/theme/header/header.component.ts
@@ -1,13 +1,20 @@
-import { Component,Input  } from '@angular/core';
+import { Component, Input  } from '@angular/core';
+
+import { AuthenticationService } from '../../_services/authentication.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+    styleUrls: ['./header.component.scss'],
+    providers: [AuthenticationService]
 })
 export class HeaderComponent {
-	@Input() isInnerPage: boolean;
-    constructor() {
-    	
+    @Input() isInnerPage: boolean;
+    constructor(private authenticationService: AuthenticationService) {
+
+    }
+
+    private logoutUser() {
+        this.authenticationService.logout();
     }
 }
