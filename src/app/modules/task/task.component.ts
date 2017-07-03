7407a8a0e21c2170c9e6c6a0c89e91e65fd9ad79
hari <hari@qburst.com>
2017-07-03 18:33:23 +0530

    completed task listing screen

--- src/app/modules/task/task.component.ts
+++ src/app/modules/task/task.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'task',
-  templateUrl: './task.component.html'
+    selector: 'task',
+    templateUrl: './task.component.html',
+    styleUrls: ['./task.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 
-export class TaskComponent { 
-	title = 'Task';
+export class TaskComponent {
+    taskList: Array < any > ;
+    constructor() {
+        this.initTaskList();
+    }
+
+    private initTaskList(): void {
+        this.taskList = [{
+            taskId: 36371,
+            name: "Install Windows In Machine",
+            dueDate: "2017-04-21T23:59:59",
+            burnedHours: "0/5",
+            estimatedHours: 18000000, // Multiples of 3600000 should show as (burnedHours/3600000)/(estimatedHours/3600000)
+            status: "Delayed",
+            PercentageCompleted: 6.67,
+            createDate: "2017-04-20T06:56:31.82",
+            isAssigned: true
+        }, {
+            taskId: 36372,
+            name: "Task1",
+            dueDate: "2017-04-29T23:59:59",
+            burnedHours: "6/8",
+            estimatedHours: 18000000,
+            status: "Delayed",
+            PercentageCompleted: 47.78,
+            createDate: "2017-04-20T06:56:31.82",
+            isAssigned: true
+        },{
+            taskId: 36373,
+            name: "Testing for description length",
+            dueDate: "2017-05-29T23:59:59",
+            burnedHours: "12/12",
+            estimatedHours: 18000000,
+            status: "Completed",
+            PercentageCompleted: 17.78,
+            createDate: "2017-04-20T06:56:31.82",
+            isAssigned: false
+        },{
+            taskId: 36374,
+            name: "Task2",
+            dueDate: "2017-04-29T23:59:59",
+            burnedHours: "12/12",
+            estimatedHours: 18000000,
+            status: "Completed",
+            PercentageCompleted: 7.78,
+            createDate: "2017-04-20T06:56:31.82",
+            isAssigned: false
+        },{
+            taskId: 36375,
+            name: "Test Task",
+            dueDate: "2017-05-02T23:59:59",
+            burnedHours: "6/12",
+            estimatedHours: 18000000,
+            status: "Delayed",
+            PercentageCompleted: 87.78,
+            createDate: "2017-04-20T06:56:31.82",
+            isAssigned: true
+        },{
+            taskId: 36376,
+            name: "Testing attachment is Done",
+            dueDate: "2017-06-02T23:59:59",
+            burnedHours: "5/12",
+            estimatedHours: 18000000,
+            status: "Delayed",
+            PercentageCompleted: 47.78,
+            createDate: "2017-05-20T06:56:31.82",
+            isAssigned: false
+        }];
+    }
 }
--- src/app/modules/task/task.module.ts
+++ src/app/modules/task/task.module.ts
@@ -2,18 +2,21 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { MdlModule } from '@angular-mdl/core';
  
 import { TaskComponent }     from './task.component';
  
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MdlModule
   ],
   declarations: [
     TaskComponent
   ],
-  bootstrap: [ TaskComponent ]
+  bootstrap: [ TaskComponent ],
+  exports: [MdlModule]
 })
 
 export class TaskModule {}
