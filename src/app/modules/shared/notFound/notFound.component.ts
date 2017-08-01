95a03d01362f72739c3ecfe5d156cc722921df8d
Midhun <midhun@qburst.com>
2017-08-01 19:43:19 +0530

    style changes

--- src/app/modules/shared/notFound/notFound.component.ts
+++ src/app/modules/shared/notFound/notFound.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'not-found',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
     styleUrls: ['./notFound.component.scss']
 })
 export class NotFoundComponent {
-    constructor() {
+    constructor(private router: Router) {
+    }
+    goBack(){
+      this.router.navigate(['/zb/home']);
     }
 }
